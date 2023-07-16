29ad3648cc1339f3370c7465512ad630a5b3717c
Shak94 <shakoi.1994@gmail.com>
2023-07-16 13:37:56 -0400

    Working on home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,8 @@ from django.urls import path, include
 urlpatterns = [
   path('', views.Home.as_view(), name="home"),
   path('cocktail/',views.CocktailMenu.as_view(), name="cocktail_menu"),
-  
+  path('mocktail/',views.MocktailMenu.as_view(), name="cocktail_menu"),
+  path('shooter/',views.ShooterMenu.as_view(), name="cocktail_menu"),
+
  
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.views import View 
 from django.views.generic.base import TemplateView
+from. models import Drink
 # Create your views here.
 class Home(TemplateView):
     template_name ="home.html"
       
     
 class CocktailMenu(TemplateView):
-    template_name= 'cocktails_menu.html'
+    template_name= 'cocktail_menu.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["cocktails"] = CocktailMenu.objects.all()
+        context["cocktails"] = Drink.objects.all()
+        return context
+
+class MocktailMenu(TemplateView):
+    template_name= 'mocktail_menu.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["mocktails"] = Drink.objects.all()
+        return context
+
+class ShooterMenu(TemplateView):
+    template_name= 'shooter_menu.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["shooters"] = Drink.objects.all()
         return context
